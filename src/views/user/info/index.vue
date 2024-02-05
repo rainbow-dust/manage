c0ee9ba584eb10e277f0774e87a33d91d49c2cc1
huamurui <huamurui@outlook.com>
2024-02-05 16:21:57 +0800

    style: code format

--- src/views/user/info/index.vue
+++ src/views/user/info/index.vue
@@ -22,60 +22,59 @@
 </template>
 
 <script lang="ts" setup>
-import UserInfoHeader from './components/user-info-header.vue';
-import LatestNotification from './components/latest-notification.vue';
-import LatestActivity from './components/latest-activity.vue';
+  import UserInfoHeader from './components/user-info-header.vue';
+  import LatestNotification from './components/latest-notification.vue';
+  import LatestActivity from './components/latest-activity.vue';
 </script>
 
 <script lang="ts">
-export default {
-  name: 'Info',
-};
+  export default {
+    name: 'Info',
+  };
 </script>
 
 <style scoped lang="less">
-.container {
-  padding: 0 20px 20px 20px;
-}
-
-.content {
-  display: flex;
-  margin-top: 12px;
-
-  &-left {
-    flex: 1;
-    margin-right: 16px;
-    overflow: hidden;
-    // background-color: var(--color-bg-2);
-
-    :deep(.arco-tabs-nav-tab) {
-      margin-left: 16px;
-    }
+  .container {
+    padding: 0 20px 20px;
   }
 
-  &-right {
-    width: 332px;
-  }
-
-  .tab-pane-wrapper {
-    padding: 0 16px 16px 16px;
-  }
-}
-</style>
-
-<style lang="less" scoped>
-.mobile {
   .content {
-    display: block;
+    display: flex;
+    margin-top: 12px;
 
     &-left {
-      margin-right: 0;
-      margin-bottom: 16px;
+      flex: 1;
+      margin-right: 16px;
+      overflow: hidden;
+      // background-color: var(--color-bg-2);
+      :deep(.arco-tabs-nav-tab) {
+        margin-left: 16px;
+      }
     }
 
     &-right {
-      width: 100%;
+      width: 332px;
+    }
+
+    .tab-pane-wrapper {
+      padding: 0 16px 16px;
     }
   }
-}
+</style>
+
+<style lang="less" scoped>
+  .mobile {
+    .content {
+      display: block;
+
+      &-left {
+        margin-right: 0;
+        margin-bottom: 16px;
+      }
+
+      &-right {
+        width: 100%;
+      }
+    }
+  }
 </style>
--- src/views/user/setting/index.vue
+++ src/views/user/setting/index.vue
@@ -22,32 +22,32 @@
 </template>
 
 <script lang="ts" setup>
-import UserPanel from './components/user-panel.vue';
-import BasicInformation from './components/basic-information.vue';
-import SecuritySettings from './components/security-settings.vue';
+  import UserPanel from './components/user-panel.vue';
+  import BasicInformation from './components/basic-information.vue';
+  import SecuritySettings from './components/security-settings.vue';
 </script>
 
 <script lang="ts">
-export default {
-  name: 'Setting',
-};
+  export default {
+    name: 'Setting',
+  };
 </script>
 
 <style scoped lang="less">
-.container {
-  padding: 0 20px 20px 20px;
-}
+  .container {
+    padding: 0 20px 20px;
+  }
 
-.wrapper {
-  padding: 20px 0 0 20px;
-  min-height: 580px;
-  background-color: var(--color-bg-2);
-  border-radius: 4px;
-}
+  .wrapper {
+    min-height: 580px;
+    padding: 20px 0 0 20px;
+    background-color: var(--color-bg-2);
+    border-radius: 4px;
+  }
 
-:deep(.section-title) {
-  margin-top: 0;
-  margin-bottom: 16px;
-  font-size: 14px;
-}
+  :deep(.section-title) {
+    margin-top: 0;
+    margin-bottom: 16px;
+    font-size: 14px;
+  }
 </style>
